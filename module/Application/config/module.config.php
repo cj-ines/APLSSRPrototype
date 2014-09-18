c0e3a7655c2a7d509aa96c495855422ec0cc7152
Crisdel James Ines <cj.ines@outlook.ph>
2014-09-18 15:11:38 +0800

    reason is required

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -88,6 +88,7 @@ return array(
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
+            'zfc-user' => __DIR__ . '/../view',
         ),
     ),
     'navigation' => array(
--- public/js/reporting/dashboard/dashboard.js
+++ public/js/reporting/dashboard/dashboard.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
 	$('.score-label').dblclick(function() {
+		$('.error-message').hide();
 		$('#scoreModal').modal('show');
 	}).tooltip();
 	$('.mgr-comment').popover();
+
+	$('.update-score-btn').click(function() {
+		var reason = $('.score-reason-control').val();
+		if (reason == '') {
+			$('.error-message').show();
+		} else {
+			$('#scoreModal').modal('hide');
+		}
+	})
 })
